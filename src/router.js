36f6fce32fe7ad24a00504decee36a8d5d78ff58
zhisun0628 <zhis1@student.unimelb.edu.au>
2023-03-11 17:11:12 +1100

    changed public path

--- src/router.js
+++ src/router.js
@@ -7,6 +7,8 @@ export const router = createRouter({
     history:createWebHistory(),
     routes:[
         {path: '/', component:HomePage},
-        {path: '/:username', component:StreamRoom}
+        {path: '/:username', component:StreamRoom},
+        {path: '/TwitchDanmu', component:HomePage},
+        {path: '/TwitchDanmu/:username', component:StreamRoom}
     ]
 })
